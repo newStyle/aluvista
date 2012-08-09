3c759904a078ef5850a2006187b3b08ede27172f
naserr <naser0rahmani@gmail.com>
2012-08-09 10:37:33 +0430

    Add Under Constraction!

---
+++ htdocs/Under Construction/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+	<meta charset="utf-8" />
+	<title>..::ALUVISTA::..</title>
+
+	<link rel="stylesheet/less" type="text/css" href="style.less" />
+	<script src="scripts/less.js" type="text/javascript"></script>
+	<script type="text/javascript" src="scripts/html5shiv.js"></script>
+	<script type="text/javascript"> document.write('<script type="text/javascript" src=scripts/' + ('__proto__' in {} ? 'zepto' : 'jquery') + '.js><\/script>'); </script>
+	<script type="text/javascript" src="scripts/underdev.js"></script>
+</head>
+</head>
+<body>
+	<header></header>
+	<section class='slideshow'>
+		<div class="progress">  <!-- Hide progress when slider is on -->
+			<p class="indent">7%</p>
+		</div>
+		<div class="logo">
+			<h1 class="indent">Aluvista</h1>
+		</div>
+		<div class="noise"></div>
+		<section class="backimg">
+			<script type="text/javascript">
+for(var i=1;i<2;i++) document.write('<div style="background-image :url(images/gallery/'+i+'.jpg);"></div>');			</script>
+		</section>
+	</section>
+	<footer>
+		<h1>Under Constructio<a href="http://www.aluvista.com/old">n</a></h1>
+	</footer>
+</body>
+</html>
