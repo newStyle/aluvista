61d457a634cf8ea612370893a8fe7971c5c2ba1b
Masoud Salman <goldgoldy276@gmail.com>
2012-09-02 00:48:16 +0430

    khone tekoni ! :D

--- seeking/scripts/script.js
+++ seeking/scripts/script.js
@@ -1,7 +1,8 @@
-$(function(){
-	 $("section.container > .down").wrapAll("<section id='con'></section>");
-	 $("header .down nav li a").click(function(e){
-	 	e.preventDefault();
-		$("#con").hide().load($(this).attr("href")).fadeIn("normal");
-	 });
+$(function () {
+	$("section.container > .down").wrapAll("<section id='con'></section>");
+	$("header .down nav li a").click(function (e) {
+		e.preventDefault();
+		$href = $(this).attr("href");
+		$("#con").hide().load($href).fadeIn("normal");
+	});
 });
