ab31296744f67584df4dc0828c6a7507f2ffb138
Masoud Salman <goldgoldy276@gmail.com>
2012-09-02 08:57:24 +0430

    styling active menu !

--- seeking/scripts/script.js
+++ seeking/scripts/script.js
@@ -1,8 +1,14 @@
 $(function () {
 	$("section.container > .down").wrapAll("<section id='con'></section>");
+
 	$("header .down nav li a").click(function (e) {
 		e.preventDefault();
 		$href = $(this).attr("href");
 		$("#con").hide().load($href).fadeIn("normal");
+	});
+
+	$("header .right .down nav a").click(function () {
+		$sibl = $(this).addClass("active");
+		$sibl.parent().siblings().children().removeClass("active");
 	});
 });
