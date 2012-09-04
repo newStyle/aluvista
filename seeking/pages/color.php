5ac66a28c8afd040cf1673ed9b597560336f463c
Masoud Salman <goldgoldy276@gmail.com>
2012-09-04 13:37:52 +0430

    update and fix bug in page color chart !

--- seeking/pages/color.php
+++ seeking/pages/color.php
@@ -1,12 +1,31 @@
 <section class="mt12 down pt1 color">
 	<section class="mla mra mt11">
 		<section class="w6 left">
-			<div class="top"></div>
-			<div class="mt5 down"></div>
+			<div class="top">
+				<article>
+					<h4>Order Your Color </h4>
+					<p>Diam aliquam enim et mus,
+						Sagittis, amet, tincidunt velit.
+					</p>
+				</article>
+			</div>
+			<div class="mt5 down">
+				<div></div>
+			</div>
 		</section>
 		<section class="right">
-			<div class="top"></div>
-			<div class="mt5 down"></div>			
+			<div class="top">
+				<article>
+					<h4>Ultricies purus! Arcu a.</h4>
+					<p>Massa aenean porttitor a turpis augue massa ut enim egestas platea et dis! Porta diam.
+						Amet, urna porta vut auctor augue? Diam ac? Dictumst et dis velit amet sit.
+						Risus, tortor scelerisque in porta diam et placerat enim a rhoncus, purus integer adipiscing.
+						Nascetur adipiscing enim pulvinar sit! Rhoncus lorem porttitor elit. Et, enim quis odio natoque.
+						Platea porttitor risus magnis facilisis porttitor dictumst, elementum tempor? Amet, adipiscing! Montes! A. Enim
+					</p>
+				</article>
+			</div>
+			<div class="mt5 down"></div>
 		</section>
 	</section>
 </section>
