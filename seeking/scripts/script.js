51a23374ed040c4b7e32528f6eb1cf55d002362e
Masoud Salman <goldgoldy276@gmail.com>
2012-09-06 09:14:43 +0430

    my jq code modified !

--- seeking/scripts/script.js
+++ seeking/scripts/script.js
@@ -1,33 +1,31 @@
-$(function () {
-	$("section.container > .down").wrapAll("<section id='con'></section>");
+var codeStationJq = {
+	ready: function () {
+		$("section.container > .down").wrapAll("<section id='con'></section>");
 
-	/* ajax load ! */
-	$("header .down nav li a").click(function (e) {
-		e.preventDefault();
-		$href = $(this).attr("href");
-		$("#con").hide().load($href).fadeIn("normal");
-	});
+		/* ajax load ! */
+		$ajaxPath = [ /* this path load in #con => section.container > .down > #con ;)*/ 
+			"header .down nav li a", 
+			"footer .down nav li a"
+		];
+		for (i = 0; i < $ajaxPath.length; i++)
+			$($ajaxPath[i]).click(function (e) {
+				e.preventDefault();
+				$href = $(this).attr("href");
+				$("#con").hide().load($href).fadeIn("normal");
+			});
 
-	$("footer .down nav li a").click(function (e) {
-		e.preventDefault();
-		$href = $(this).attr("href");
-		$("#con").hide().load($href).fadeIn("normal");
-	});
+		/* active menu ! */
+		$pathMenu = [ /* this path can active menu in page ;)*/ 
+			"header .right .down nav a",
+			"section.container .top nav a",
+			"footer .down nav a"
+		];
+		for (i = 0; i < $pathMenu.length; i++)
+			$($pathMenu[i]).click(function () {
+				$sibl = $(this).addClass("active");
+				$sibl.parent().siblings().children().removeClass("active");
+			});
+	}
+};
 
-	/* active menu */
-	$("header .right .down nav a").click(function () {
-		$sibl = $(this).addClass("active");
-		$sibl.parent().siblings().children().removeClass("active");
-	});
-
-	$("section.container .top nav a").click(function(){
-		$sibl = $(this).addClass("active");
-		$sibl.parent().siblings().children().removeClass("active");
-	});
-
-	$("footer .down nav a").click(function () {		
-		$sibl = $(this).addClass("active");
-		$sibl.parent().siblings().children().removeClass("active");
-	});
-
-});
+$(document).ready(codeStationJq.ready);
