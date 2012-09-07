cafdf6ef296a2ff1bbff953f5c350fc2a5806844
naserr <naser0rahmani@gmail.com>
2012-09-08 03:28:25 +0430

    fix index

--- seeking/pages/index.php
+++ seeking/pages/index.php
@@ -1,28 +1,31 @@
-<section class="mt12 down index">
-	<section class="w6 left"></section>
-	<section class="ml6 right">
-		<section class="w16 mra ">
-			<div class="left">
-				<div class="top"></div>
-				<div class="down">
-					<p>Nunc elementum adipiscing.
-						Urna, Ridiculus. </p>
+	<section class="mt12 down index">
+		<div class="recent w6 left"></div>
+		<section class="ml7 right">
+			<section class="w16 mra ">
+				<div class="left">
+					<div class="top"></div>
+					<div class="down">
+						<p>Nunc elementum adipiscing.
+							Urna, Ridiculus. </p>
+					</div>
 				</div>
-			</div>
-			<div class="right">
-				<div class="top"></div>
-				<div class="down">
-					<p>Nec quis integer tincidunt.
-						Pulvinar scelerisque dis.</p>
+				<div class="right">
+					<div class="top"></div>
+					<div class="down">
+						<p>Nec quis integer tincidunt.
+							Pulvinar scelerisque dis.</p>
+					</div>
 				</div>
-			</div>
-			<div class="mla mra middle">
-				<div class="top"></div>
-				<div class="down">
-					<p>Natoque, aenean tortor mattis tortor.
-						Platea a cursus nec.</p>
+				<div class="mla mra middle">
+					<div class="top"></div>
+					<div class="down">
+						<p>Natoque, aenean tortor mattis tortor.
+							Platea a cursus nec.</p>
+					</div>
 				</div>
-			</div>
+			</section>
 		</section>
 	</section>
+	<!-- End Content --> 
 </section>
+<!-- End Container -->
