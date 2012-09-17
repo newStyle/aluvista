1d345a2c2565a078aaa5f96ec7d7db2476248d99
naserr <naser0rahmani@gmail.com>
2012-09-17 22:55:33 +0430

    add seeking folder to my works

---
+++ naserr/home.php
@@ -0,0 +1,139 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>..: Aluvista :..</title>
+<link rel="stylesheet/less" type="text/css" href="reset.css" />
+<link rel="stylesheet/less" type="text/css" href="1styles.css" />
+<link rel="stylesheet/less" type="text/css" href="style.less" />
+<script type="text/javascript" src="scripts/less.js"></script>
+<script type="text/javascript" src="scripts/jquery.js"></script>
+<script type="text/javascript" src="scripts/html5.js"></script>
+<script type="text/javascript" src="scripts/script.js" ></script>
+<!-- <link type="text/css" rel="stylesheet" href="font/font.css" /> -->
+</head>
+<body>
+<header>
+<section class="mla mra">
+<div class="w10 right">
+	<div class="w10 pt1 top">
+		<div class="left w9">
+			<nav>
+				<ul>
+					<li><a href="#">Chat</a></li>
+					<li><a href="#">Call</a></li>
+				</ul>
+			</nav>
+		</div>
+		<div class="right ml9"></div>
+	</div>
+	<div class="w10 mt2 down">
+		<nav>
+			<ul>
+				<li><a href="pages/index.php">home</a></li>
+				<li><a href="pages/process.php">process</a></li>
+				<li><a href="pages/gallery.php">gallery</a></li>
+				<li><a href="pages/color.php">color</a></li>
+				<li class="right"><a href="pages/contact.php">contact</a></li>
+			</ul>
+		</nav>
+	</div>
+</div>
+<div class="w4 ml1 left"></div>
+</section>
+</header>
+<!-- End header -->
+<section class="container mla mra mt9">
+	<section class="slider top">
+		<section class=" w23 mla mra">
+			<section class="w6 left">
+				<nav>
+					<ul>
+						<li><a href="#"  class="active">Learn Lpsum Dolar Sit</a></li>
+						<li><a href="#">Learn Lpsum Dolar Sit</a></li>
+						<li><a href="#">Learn Lpsum Dolar Sit</a></li>
+						<li><a href="#">Learn Lpsum Dolar Sit</a></li>
+						<li><a href="#">Learn Lpsum Dolar Sit</a></li>
+					</ul>
+				</nav>
+			</section>
+			<section class="right last">
+				<div class="top">
+					<p> Amet ac placerat platea dolor.
+						Et platea tincidunt scelerisque.
+						Egestas, et, ridiculus, purus <br />
+						<a href="#">more...</a> </p>
+				</div>
+				<div class="down">
+					<p> Turpis sed dignissim a sed.
+						Phasellus quis sed massa.
+						Porta! Sagittis a sagittis <br />
+						<a href="#">more...</a> </p>
+				</div>
+			</section>
+			<section class="w11 middle mla mra"></section>
+		</section>
+	</section>
+	<!-- End Slider -->
+	<section class="mt12 down index">
+		<div class="recent w6 left"></div>
+		<section class="ml7 right">
+			<section class="w16 mra ">
+				<div class="left">
+					<div class="top"></div>
+					<div class="down">
+						<p>Nunc elementum adipiscing.
+							Urna, Ridiculus. </p>
+					</div>
+				</div>
+				<div class="right">
+					<div class="top"></div>
+					<div class="down">
+						<p>Nec quis integer tincidunt.
+							Pulvinar scelerisque dis.</p>
+					</div>
+				</div>
+				<div class="mla mra middle">
+					<div class="top"></div>
+					<div class="down">
+						<p>Natoque, aenean tortor mattis tortor.
+							Platea a cursus nec.</p>
+					</div>
+				</div>
+			</section>
+		</section>
+	</section>
+	<!-- End Content --> 
+</section>
+<!-- End Container -->
+<footer>
+	<div class="top">
+		<div class="dirt container mla mra ">
+			<div class="right w1 rss"></div>
+			<div class="right w5">
+				<nav>
+					<ul>
+						<li><a href="#">Twitter</a></li>
+						<li><a href="#">Facebook</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</div>
+	<!-- End top-footer -->
+	<div class="container down">
+		<nav>
+			<ul>
+				<li><a href="pages/index.php">home</a></li>
+				<li><a href="pages/process_content.php">process</a></li>
+				<li><a href="pages/gallery.php">gallery</a></li>
+				<li><a href="pages/color.php">color</a></li>
+				<li class="right active"><a href="pages/contact.php">contact</a></li>
+			</ul>
+		</nav>
+	</div>
+</footer>
+<!-- End footer -->
+<div class="index"></div>
+</body>
+</html>
