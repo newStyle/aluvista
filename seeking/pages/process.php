f17dfd03f82c14a67fc1aef919c020759264988f
naserr <naser0rahmani@gmail.com>
2012-09-20 04:42:23 +0330

    Create Process page Part1

--- seeking/pages/process.php
+++ seeking/pages/process.php
@@ -1,7 +1,15 @@
-<section class="mt2 mb11 process">
+<section class="mt2 mb11 down process">
 	<section class="mla mra mt11">
-		<section class="w6 left"> 
-			<div class="content">	
+		<section class="content mla mra"> 
+			<div class="w12 right">
+				<li class="one"></li>
+				<li class="two"></li>
+				<li class="three"></li>
+				<li class="four"></li>
+				<li class="five"></li>
+				<li class="six"></li>
+				<li class="seven"></li>
+				<li class="eight"></li>
 			</div>
 		</section> 
 	</section>-->
