b004950cf3c2b47ad92c2af66858f1d9f908b59e
Masoud Salman <goldgoldy276@gmail.com>
2012-09-23 07:16:55 +0330

    [update] set size ...

--- seeking/pages/gallery.php
+++ seeking/pages/gallery.php
@@ -1,5 +1,5 @@
 <section class="mt10 mb12 down gallery">
-	<section class="w21 mt8 mla mra pt1"><!--
+	<section class="w22 mt8 mla mra pt1">
 		<section class="w6 mt12 ml1 left">
 			<p> our factory Gallary </p>
 			<section>
@@ -33,10 +33,9 @@
 		</section>
 		<section class="right pt1">
 			<p class="right mt5"><span><a href="#">play</a></span>/<span><a href="#">puase</a></span></p>
-			<div class="w11 top mt12">
-				
+			<div class="w13 top mt12">
 			</div>
 			<div class="w11 mt7 down"></div>
-		</section>-->
+		</section>
 	</section> 
 </section>
