bd61e87b599fdc31091fc54bcec8ae148b813835
Masoud Salman <goldgoldy276@gmail.com>
2012-11-04 14:31:10 +0330

    [update] reset Works !!

--- seeking/pages/process.php
+++ seeking/pages/process.php
@@ -1,17 +1,7 @@
 <section class="mt2 mb11 down process">
 	<section class="mla mra mt11">
 		<section class="content mla mra"> 
-			<div class="w12 right">
-				<li class="one"></li>
-				<li class="two"></li>
-				<li class="three"></li>
-				<li class="four"></li>
-				<li class="five"></li>
-				<li class="six"></li>
-				<li class="seven"></li>
-				<li class="eight"></li>
-				<li class="wheel"></li>
-			</div>
+			<div class="w12 right" id="disk"></div>
 		</section> 
 	</section>
 </section>
