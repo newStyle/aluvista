b3598af789883bb66f3349552874faa2db3a5532
naserr <naser0rahmani@gmail.com>
2012-11-18 19:22:06 +0330

    create product page

--- seeking/pages/product.php
+++ seeking/pages/product.php
@@ -1,3 +1,48 @@
-<h1 style="text-align: center; color: magenta">
-	under construction !!
-</h1>
+<section class="mt2 mb11 b down pt1 pdt">
+	<section class="mla mra mt11">
+		<section class="w6 left">
+			<div class="top">
+				<article>
+					<h4>روش های نصب</h4>
+					<p>
+						در این قسمت با روش نصب ورق کامپوزیت آشنا می شوید
+					</p>
+				</article>
+			</div>
+			<div class="mt8 down pt1">
+				<section id="btn-pdt">
+					<div><input type="button" name="pt1" value="آماده سازی قاب" class="active"/></div>
+					<div><input type="button" name="pt2" value="آماده سازی کامپوزیت"/></div>
+					<div><input type="button" name="pt3" value="برش کنج ها"/></div>
+					<div><input type="button" name="pt4" value="جعبه سازی و تا زدن"/></div>
+					<div><input type="button" name="pt5" value="تقویت"/></div>
+					<div><input type="button" name="pt6" value="بالا بردن پانل و نصب آن"/></div>
+					<div><input type="button" name="pt7" value="پر کدن درز با چسب"/></div>
+					<div><input type="button" name="pt8" value="جدا کردن لایه محافظ"/></div>
+				</section>
+			</div>
+		</section>
+		<section class="right">
+			<section class="top">
+				<article>
+					<h4>اطلاعات مربوط به صفحه تولیدات</h4>
+					<p>
+صفحات نماي ساختمان
+نوسازس تزئيني بناهاي قديمي اي كه طبقاتي به آنها اضافه شده است.
+تزيين ديوارهاي داخلي –سقف ها – حمام ها- آشپزخانه ها و بالكن ها
+تزئين در فروشگاهها
+تابلوهاي تبليغاتي –غرفه نمايشگاهها و تابلوهاي سر در
+پوشش جداره و سقف تونل ها
+مواد صنعتي و مواد وسايل نقليه و قايق ها</p>
+				</article>
+			</section>
+			<section class="mt12 down pt1">
+				<section class="w12-1 ml1-1 mra"></section>
+				<div id='scrollBar'>
+					<div id="button"></div>
+					<div id='ribbon'></div>
+				</div>
+			</section>
+		</section>
+	</section>
+</section>
