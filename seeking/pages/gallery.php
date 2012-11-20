a97be91368f4dc7b2f05c1798ebbf2df0cb98a49
naserr <naser0rahmani@gmail.com>
2012-11-20 17:33:09 +0330

    change bg of Scroll

--- seeking/pages/gallery.php
+++ seeking/pages/gallery.php
@@ -1,12 +1,12 @@
 <section class="mt10 mb12 down gallery">
 	<section class="w22 mt8 mla mra pt1">
 		<section class="w6 mt12 ml1 left">
-			<p> our factory Gallary </p>
+			<p>مجموعه پروژه های مسکونی</p>
 			<section></section><!-- small pic -->
 			<div class="w3 paging"></div>
 		</section>
 		<section class="right pt1">
-			<p class="right mt5"><span><a href="#play">play</a></span>/<span><a href="#puase">puase</a></span></p>
+			<p class="right mt5"><span><a href="#play">اجرا</a></span>/<span><a href="#puase">ایست</a></span></p>
 			<div class="w13 top mt12"></div>
 			<div class="w11 mt7 down"></div>
 		</section>
