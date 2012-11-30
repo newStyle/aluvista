ed71dc1263503bbcdf4ee22fae8437d10fe2a187
naserr <naser0rahmani@gmail.com>
2012-12-01 03:27:32 +0330

    create new btns for Gallery page

--- seeking/pages/gallery.php
+++ seeking/pages/gallery.php
@@ -3,10 +3,17 @@
 		<section class="w6 mt12 ml1 left">
 			<p>مجموعه پروژه های مسکونی</p>
 			<section></section><!-- small pic -->
-			<div class="w3 paging"></div>
+			<div class="w2-1 left paging"></div>
+			<div class="right next"><a href="#"></a></div>
+			<div class="left prev"><a href="#"></a></div>
 		</section>
 		<section class="right pt1">
-			<p class="right mt2"><span><a href="#play">اجرا</a></span>/<span><a href="#puase">ایست</a></span></p>
+			<div id="gbtn" class="right mt2">
+				<div class="left play"><a href="#play"></a></div>
+				<div class="left puase"><a href="#puase"></a></div>
+				<div class="left prev"><a href="#prev"></a></div>
+				<div class="left next"><a href="#next"></a></div>
+			</div>
 			<div class="w13 top mt12"></div>
 			<div class="w11 mt7 down"></div>
 		</section>
