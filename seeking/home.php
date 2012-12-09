0853733194e2fec434208f34157b790b49d68db9
Masoud Salman <goldgoldy276@gmail.com>
2012-12-09 20:56:53 +0330

    [update] include used for create home page... !!

--- seeking/home.php
+++ seeking/home.php
@@ -1,54 +1,4 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-<meta charset="utf-8" />
-<title>..: Aluvista :..</title>
-<link type="text/css" rel="stylesheet" href="font.css" />
-<link rel="stylesheet/less" type="text/css" href="reset.css" />
-<link rel="stylesheet/less" type="text/css" href="1styles.css" />
-<link rel="stylesheet/less" type="text/css" href="style.less" />
-<script type="text/javascript" src="scripts/less.js"></script>
-<script type="text/javascript" src="scripts/jquery.js"></script>
-<script type="text/javascript" src="scripts/html5.js"></script>
-<script type="text/javascript" src="scripts/script.js" ></script>
-<!--[if IE]>
-		<script type="text/javascript">
-			$(function(){
-			$("header").css({ 'background':'url(images/header_bg.png) repeat top left'});
-			});
-		</script>
-<![endif]-->
-</head>
-<body>
-<header>
-<section class="mla mra">
-<div class="w14 right">
-	<div class="w15 pt1 top">
-		<div class="left w12 ">
-			<div id="searchBox">
-				<input type="search" id="schh"  name="search" class="search" placeholder="بـــــــــــــــــــــگرد" />
-				<input type="submit" id="smt"  name="submit" class="submit" value=''/>
-		    </div>
-		</div>
-		<div class="right ml13"></div>
-	</div>
-	<div class="w14 mt2 down">
-		<nav>
-			<ul>
-				<li><a href="pages/index.php">صفحه اصلی</a></li>
-				<li><a href="pages/process.php">مراحل ساخت</a></li>
-				<li><a href="pages/product.php">تولیدات</a></li>
-				<li><a href="pages/gallery.php">گالری</a></li>
-				<li><a href="pages/color.php">جدول رنگ</a></li>
-				<li class="left"><a href="pages/contact.php">تماس با ما</a></li>
-			</ul>
-		</nav>
-	</div>
-</div>
-<a id="logo" class="w4 ml1 left" href="pages/index.php"></a>
-</section>
-</header>
-<aside id='srchPanel'></aside>
+<?php include('./inc/header.php');?>
 <!-- End header -->
 <section class="container mla mra mt9">
 	<section class="slider top">
@@ -108,40 +58,4 @@
 	<!-- End Content --> 
 </section>
 <!-- End Container -->
-<footer>
-	<div class="top">
-		<div class="dirt container mla mra ">
-			<div class="left">
-				<nav>
-					<ul>
-						<li><a href="http://twitter.com">Twitter</a></li>
-						<li><a href="http://facebook.com">Facebook</a></li>
-					</ul>
-				</nav>
-			</div>
-			<div class="last mla mr18 w1 rss"></div>
-		</div>
-	</div>
-	<!-- End top-footer -->
-	<div class="container down">
-		<nav>
-			<ul>
-				<li><a href="pages/index.php">صفحه اصلی</a></li>
-				<li><a href="pages/process.php">مراحل ساخت</a></li>
-				<li><a href="pages/product.php">تولیدات</a></li>
-				<li><a href="pages/gallery.php">گالری</a></li>
-				<li><a href="pages/color.php">جدول رنگ</a></li>
-				<li class="left"><a href="pages/contact.php">تماس با ما</a></li>
-			</ul>
-		</nav>
-	</div>
-</footer>
-<!-- End footer -->
-<div class="loading">
-	<div class="load">
-		<p>لطفا منتظر باشید</p>
-		<img src="images/loading.gif" alt="">
-	</div>
-</div>
-</body>
-</html>
+<?php include('./inc/footer.php');?>
---
+++ seeking/inc/footer.php
@@ -0,0 +1,37 @@
+<footer>
+	<div class="top">
+		<div class="dirt container mla mra ">
+			<div class="left">
+				<nav>
+					<ul>
+						<li><a href="http://twitter.com">Twitter</a></li>
+						<li><a href="http://facebook.com">Facebook</a></li>
+					</ul>
+				</nav>
+			</div>
+			<div class="last mla mr18 w1 rss"></div>
+		</div>
+	</div>
+	<!-- End top-footer -->
+	<div class="container down">
+		<nav>
+			<ul>
+				<li><a href="pages/index.php">صفحه اصلی</a></li>
+				<li><a href="pages/process.php">مراحل ساخت</a></li>
+				<li><a href="pages/product.php">تولیدات</a></li>
+				<li><a href="pages/gallery.php">گالری</a></li>
+				<li><a href="pages/color.php">جدول رنگ</a></li>
+				<li class="left"><a href="pages/contact.php">تماس با ما</a></li>
+			</ul>
+		</nav>
+	</div>
+</footer>
+<!-- End footer -->
+<div class="loading">
+	<div class="load">
+		<p>لطفا منتظر باشید</p>
+		<img src="images/loading.gif" alt="">
+	</div>
+</div>
+</body>
+</html>
