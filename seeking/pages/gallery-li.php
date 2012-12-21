1ef068fe973aa76421b9f0b5faee8e589d6fcba0
Masoud Salman <goldgoldy276@gmail.com>
2012-12-21 20:24:18 +0330

    [update] ...a f a r i a n mikoshammet x(

--- seeking/pages/gallery-li.php
+++
@@ -1,5 +0,0 @@
-<li><a href="#" class='li-gallery'>پروژه های دولتی</a></li>
-<li><a href="#" class='li-gallery'>پروژه های تجاری</a></li>
-<li><a href="#" class='li-gallery'>پروژه های خصوصی </a></li>
-<li><a href="#" class='li-gallery'>پروژه های مسکونی</a></li>
-<li><a href="#" class='li-gallery'>پروژه های هتل</a></li>
