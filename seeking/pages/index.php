46e7495815820d0353a69de84d4654692461ded9
Masoud Salman <goldgoldy276@gmail.com>
2012-12-30 19:57:19 +0330

    [fix] html base fixed for loading and other works ..

--- seeking/pages/index.php
+++ seeking/pages/index.php
@@ -1,19 +1,19 @@
-	<section class="mt12 down index">
+	<section class="mt12 down index pt5">
 		<section class="left">
 			<section class="w24 mla mra ">
-				<div class="w7 left">
+				<div class="w7 left ml1">
 					<a href="process.php"><div class="top"></div></a>
 					<div class="down">
 						<p>اطلاعات فنی و روش تولید ورق های کامپوزیت </p>
 					</div>
 				</div>
-				<div class="w7 left ml1-2 middle">
+				<div class="w7 right mr1 last">
 					<a href="product.php"><div class="top"></div></a>
 					<div class="down">
 						<p>آموزش نصب ورق کامپوزیت آلوویستا </p>
 					</div>
 				</div>
-				<div class="w7 ml17 last">
+				<div class="w7 mla mra middle">
 					<a href="color.php"><div class="top"></div></a>
 					<div class="down">
 						<p>رنگ بندی و طرح های جدید</p>
